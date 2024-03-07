2f718ae9af573268b8693cfc3d905d0446d54e20
Ayushi-gupta1225 <77430787+Ayushi-gupta1225@users.noreply.github.com>
2024-03-08 00:18:54 +0530

    Initial commit

---
+++ src/main/java/org/targetindia/day1/calender/Main.java
@@ -0,0 +1,11 @@
+package org.targetindia.day1.calender;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] input = inputreader.readInput();
+        int year = input[0];
+        int month = input[1];
+        calendergenerator.generateCalendar(year, month);
+    }
+}
---
+++ src/main/java/org/targetindia/day1/word/numtowords.java
@@ -0,0 +1,2 @@
+package org.targetindia.day1.word;public class numtowords {
+}
