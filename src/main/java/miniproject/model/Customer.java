6b0dd6a6726835bfed3b85f953081567239db52f
Ayushi-gupta1225 <77430787+Ayushi-gupta1225@users.noreply.github.com>
2024-03-15 23:59:51 +0530

    miniproject

---
+++ src/main/java/miniproject/model/Customer.java
@@ -0,0 +1,15 @@
+package miniproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    private int id;
+    private String name;
+    private String city;
+    private String email;
+    private String phone;
+}
