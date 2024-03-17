5f4fc85766e28726e3eb3a30c569cde2e4a5fd9f
Ayushi-gupta1225 <77430787+Ayushi-gupta1225@users.noreply.github.com>
2024-03-17 22:02:42 +0530

    miniproject

---
+++ src/main/java/miniproject/service/CustomerManager.java
@@ -0,0 +1,50 @@
+package miniproject.service;
+
+
+import miniproject.dao.CustomerDao;
+import miniproject.dao.DaoException;
+import miniproject.model.Customer;
+
+import java.util.List;
+
+    public class CustomerManager {
+
+        // this class has a dependency on an object of CustomerDao
+        private CustomerDao dao;
+
+        public CustomerManager() throws ServiceException {
+            // tight coupling of dependency
+            // must be converted to a loose coupling
+            // this.dao = new CsvFileCustomerDao();
+            // this.dao = new ArrayListCustomerDao();
+
+            // here is the solution; loose coupling;
+            // CustomerManager asks somebody to give an object of CustomerDao
+            try {
+                this.dao = DaoFactory.getCustomerDao();
+            } catch (DaoException e) {
+                throw new ServiceException(e);
+            }
+        }
+
+        public List<Customer> getAll() throws ServiceException {
+            try {
+                return dao.getAll();
+            }
+            catch (DaoException e){
+                throw new ServiceException(e);
+            }
+        }
+
+        public Customer addNewCustomer(Customer customer) throws ServiceException{
+            try{
+                // we can do some validation here before saving.
+
+                return dao.save(customer);
+            }
+            catch (DaoException e){
+                throw new ServiceException(e);
+            }
+        }
+    }
+
---
+++ src/main/java/miniproject/service/ServiceException.java
@@ -0,0 +1,15 @@
+package miniproject.service;
+
+
+public class ServiceException extends Exception{
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+}
