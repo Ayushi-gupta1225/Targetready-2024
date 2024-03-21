9a7ca0764389c34365ce189087f8e335784a8b25
Ayushi-gupta1225 <77430787+Ayushi-gupta1225@users.noreply.github.com>
2024-03-21 20:44:51 +0530

    file handling operations

---
+++ src/main/java/FileHandlingExamples/FISdemo.java
@@ -0,0 +1,25 @@
+package FileHandlingExamples;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Fisdemo {
+    public static void main(String[] args) {
+//
+//        String filePath = "abc.txt";
+//        File file = new File(filePath);
+//        System.out.println("Absolute Path: " + file.getAbsolutePath());
+        // Your FileInputStream code here...
+        try {
+            FileInputStream fis = new FileInputStream("abc.txt");
+            int data = fis.read();
+            System.out.println("data: " + data);
+            fis.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
+}
