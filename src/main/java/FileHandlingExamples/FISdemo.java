a63d438f92b7b6b1abc1ce614cd262fa45832fad
Ayushi-gupta1225 <77430787+Ayushi-gupta1225@users.noreply.github.com>
2024-03-22 13:39:08 +0530

    inputstream

--- src/main/java/FileHandlingExamples/FISdemo.java
+++ src/main/java/FileHandlingExamples/FISdemo.java
@@ -1,5 +1,6 @@
 package FileHandlingExamples;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,14 +8,15 @@ import java.io.IOException;
 public class Fisdemo {
     public static void main(String[] args) {
 //
-//        String filePath = "abc.txt";
-//        File file = new File(filePath);
-//        System.out.println("Absolute Path: " + file.getAbsolutePath());
+        String filePath = "abc.txt";
+        File file = new File(filePath);
+        System.out.println("Absolute Path: " + file.getAbsolutePath());
         // Your FileInputStream code here...
         try {
             FileInputStream fis = new FileInputStream("abc.txt");
             int data = fis.read();
             System.out.println("data: " + data);
+            System.out.println(" "+ (char)data);
             fis.close();
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
