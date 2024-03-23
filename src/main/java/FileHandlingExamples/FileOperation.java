3edd0f25e5bfc9bad178d77a102d6321838c634e
Ayushi-gupta1225 <77430787+Ayushi-gupta1225@users.noreply.github.com>
2024-03-23 12:06:00 +0530

    create,read,write and copy  data to binary file

--- src/main/java/FileHandlingExamples/FileOperation.java
+++ src/main/java/FileHandlingExamples/FileOperation.java
@@ -1,7 +1,6 @@
 package FileHandlingExamples;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileOperation {
     public static void main(String[] args) throws IOException {
@@ -13,6 +12,22 @@ public class FileOperation {
             else
                 System.out.println("file already exists");
 
+
+            //file writing
+           // File f2=new File("D:\\Targetassisgnment\\Targetready-2024\\src\\main\\java\\FileHandlingExamples\\cba.txt");
+            String data="101 , ayushi gupta , agra ,up";
+            FileWriter output=new FileWriter("cbc.txt");
+            output.write(data);
+            System.out.println("data is written");
+
+            //file reading
+            char[]  c=new char[100];
+            FileReader fr=new FileReader("cbc.txt");
+            fr.read(c);
+            System.out.println("data read from file");
+            System.out.println(c);
+            fr.close();
+
             //file deletion
             boolean result=f.delete();
             if(result==true)
@@ -20,6 +35,20 @@ public class FileOperation {
             else
                 System.out.println("file deletion not possible");
 
+            //character streams
+            //Reader is an abstract class for reading character streams.
+            //It's used for reading text data
+            Reader reader = new FileReader("example.txt");
+            int charstream = reader.read(); // Read a single character
+            while (charstream != -1) {
+                // Process the character
+                System.out.print((char) charstream);
+                charstream = reader.read(); // Read the next character
+            }
+            reader.close();
+
+
+            output.close();
 
         }catch(Exception e ){
             System.err.println(e);
