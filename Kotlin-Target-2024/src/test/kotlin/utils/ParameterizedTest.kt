6ce6bb5e10d5b2c1aeae2832b2cae8b3f547f7f5
Ayushi-gupta1225 <77430787+Ayushi-gupta1225@users.noreply.github.com>
2024-04-07 23:18:48 +0530

    kotlin unit testing(parameterized)

---
+++ Kotlin-Target-2024/src/test/kotlin/utils/ParameterizedTest.kt
@@ -0,0 +1,30 @@
+package utils
+
+//import org.junit.jupiter.api.Assertions.assertEquals
+import org.example.utils.helper
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+
+@RunWith(value=Parameterized::class)
+class ParameterizedTest (val input:String , val expectedval:Boolean) {
+    @Test
+    fun test() {
+        val Helper = helper()
+        val result = Helper.isPalindrome(input)
+        assertEquals(expectedval, result)
+    }
+    companion object{
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): List<Array<Any>>{
+            return listOf(
+                arrayOf("hello",false),
+                arrayOf("level",true),
+                arrayOf("a" ,true)
+            )
+        }
+    }
+}
